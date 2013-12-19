a6cae6f9053ce3285a6aaaa96bfc6da342a0459f
mizux <dev@mizux.net>
2013-12-19 10:09:37 +0100

    Adding rlist install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Warning !\nThis script won't do backup !"
+
+echo "Installing zsh configuration..."
+cp zshr ~/.zshrc
+echo "Installing zsh configuration DONE"
+
+echo "Installing vim configuration..."
+cp vimrc ~/.vimrc
+cp -r vim ~/.vim
+echo "Installing vim configuration DONE"
+
+echo "Installing aldebaran configuration..."
+mkdir -pv ~/.local/bin
+cp rlist ~/.local/bin/rlist
+echo "Installing aldebaran configuration DONE"
