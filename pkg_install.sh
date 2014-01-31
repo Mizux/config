1253cc863ba25ed10ff1c234b0ccae168a2813d9
mizux <dev@mizux.dev>
2014-01-31 15:59:31 +0100

    pkglist.txt v0.1RC

---
+++ pkg_install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+echo "install System..."
+pacman -Sy --needed base base-devel dialog  zsh gvim \
+perl-rename tree htop hwinfo acpi alsa-utils \
+xorg-server xorg-xinit xorg-server-utils mesa \
+nvidia nvidia-utils nvidia-libgl \
+awesome rxvt-unicode \
+lxappearance gtk-engine-murrine oxygen-icons oxygen-icons-svg \
+anthy uim otf-ipafont ttf-hanazono \
+ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-freefont
+echo "install System Done"
+
+echo "install Dev..."
+pacman -Sy --needed git tk aspell-en aspell-fr meld sloccount \
+qtcreator clang cmake gdb strace valgrind \
+boost bullet openscenegraph \
+doxygen graphviz qt qt4 ctags upx \
+desmume
+echo "install Dev Done"
+
+echo "install Media..."
+pacman -Sy --needed unrar unzip p7zip \
+chromium firefox flashplugin filezilla xchat \
+smplayer audacious audacious-plugins \
+scrot gimp inkscape feh gcolor2 blender wings3d \
+calibre \
+nethack wesnoth
+echo "install Media Done"
