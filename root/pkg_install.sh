980f974288d70224a0916e20af4297f7e0f38de5
mizux <dev@mizux.net>
2014-04-21 19:54:48 +0200

    Add pip2 & sphinx2

--- root/pkg_install.sh
+++ root/pkg_install.sh
@@ -14,7 +14,7 @@ echo "install System Done"
 
 echo "install Dev..."
 pacman -S --needed git tk tig aspell-en aspell-fr meld sloccount \
-ipython \
+ipython python2-pip python2-sphinx \
 qtcreator clang cmake gdb strace valgrind \
 boost bullet openscenegraph \
 doxygen graphviz qt qt4 ctags upx \
