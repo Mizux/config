f72f42086685e482ce03de432ed41390b5dd149f
mizux <dev@mizux.net>
2014-11-23 20:10:07 +0100

    add gradle

--- root/aur_install.sh
+++ root/aur_install.sh
@@ -6,5 +6,5 @@ yaourt -S --needed --noconfirm \
 android-tools android-udev-git \
 android-sdk android-sdk-platform-tools android-sdk-build-tools \
 android-platform android-ndk32 \
-android-apktool android-studio
+android-apktool android-studio gradle gradle-doc
 echo "install Android Done"
