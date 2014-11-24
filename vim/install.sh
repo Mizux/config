af8f9e7184e4c57a713c7d63d04b1b7e6b62d480
mizux <dev@mizux.net>
2014-11-24 11:10:37 +0100

    improve vim config

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 echo "Install VIM..."
 
@@ -15,7 +15,7 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 echo "Finalize YouCompleteMe install"
-#$(cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer)
-cp /usr/lib/libclang.so ~/.vim/bundle/YouCompleteMe/third_party/ycmd/.
+$(cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer --system-libclang)
+#cp /usr/lib/libclang.so ~/.vim/bundle/YouCompleteMe/third_party/ycmd/.
 
 echo "Install VIM...DONE"
