a25b412e8fab256e732a3dff2535a5e10bdd748a
mizux <dev@mizux.net>
2014-11-30 20:36:45 +0100

    Add python2 beautiful soup 4

--- root/pkg_install.sh
+++ root/pkg_install.sh
@@ -28,7 +28,8 @@ echo "install Dev Done"
 
 echo "install Python2 dev..."
 pacman -S --needed ipython2 python2-pip \
-python2-docopt python2-dateutil python2-matplotlib python2-numpy \
+python2-beautifulsoup4 python2-docopt python2-dateutil \
+python2-matplotlib python2-numpy \
 python2-paramiko python2-pytz python2-sphinx \
 echo "install Python2 dev Done"
 
