4d62ef64cd174c0cfe0828d008cf999e0ed9791d
mizux <dev@mizux.net>
2014-12-01 01:38:42 +0100

    Add python2 requests

--- root/pkg_install.sh
+++ root/pkg_install.sh
@@ -30,7 +30,7 @@ echo "install Python2 dev..."
 pacman -S --needed ipython2 python2-pip \
 python2-beautifulsoup4 python2-docopt python2-dateutil \
 python2-matplotlib python2-numpy \
-python2-paramiko python2-pytz python2-sphinx \
+python2-paramiko python2-pytz python2-requests python2-sphinx \
 echo "install Python2 dev Done"
 
 echo "install Media..."
