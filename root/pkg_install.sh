2e9bacc310362befa36f79bedd64583d21b372c0
Mizux <dev@mizux.net>
2014-12-13 14:48:41 +0100

    add zathura-djvu

--- root/pkg_install.sh
+++ root/pkg_install.sh
@@ -38,6 +38,6 @@ pacman -S --needed unrar unzip p7zip \
 chromium firefox flashplugin filezilla xchat perl-file-mimeinfo \
 smplayer vlc audacious audacious-plugins libcue libmms neon wavpack alsa-utils  \
 scrot gimp inkscape feh gcolor2 blender wings3d \
-calibre libreoffice zathura zathura-pdf-mupdf \
+calibre libreoffice zathura zathura-pdf-mupdf zathura-djvu \
 nethack wesnoth dwarffortress armagetronad
 echo "install Media Done"
