09fbc644c91a803796ad1e865a292fecb885204e
mizux <dev@mizux.net>
2015-04-17 09:18:55 +0200

    android fix spawn

--- android/android_install.sh
+++ android/android_install.sh
@@ -48,7 +48,7 @@ echo "Install all revision 19 related (android 4.4.2)"
 export PATH=~/android/android-sdk-linux/tools:$PATH
 expect -c ' set timeout -1;\
 spawn android - update sdk --all --no-ui --filter \
-platform-tools,build-tools-22.0.1,android-19;sys-img-armeabi-v7a-android-19\
+platform-tools,build-tools-22.0.1,android-19,sys-img-armeabi-v7a-android-19;\
 expect "Do you accept the license" { exp_send "y\r";exp_continue } '
 
 echo "Please add this to your bashrc..."
@@ -56,11 +56,12 @@ echo "# ANDROID"
 echo "export JAVA_HOME=/usr/lib/jvm/default"
 echo "export ANDROID_NDK_ROOT=~/android/android-ndk-r10d"
 echo "export ANDROID_SDK_ROOT=~/android/android-sdk-linux"
+#echo "export ANDROID_SDK_HOME=~"
 echo "export PATH=$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH"
 
 echo "Create Android Virtual Device rev.19 ARMv7"
 android create avd --name Default --target android-19 --abi armeabi-v7a
 echo "Test emulator..."
-emulator -avd Default
+emulator -avd Default # use ANDROID_SDK_HOME/.android for cache
 
 echo "SUCCESS !"
