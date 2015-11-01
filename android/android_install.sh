2b7bf5fd6a9af3beb56243557849d4c3c50ed36d
mizux <dev@mizux.net>
2015-11-01 23:11:42 +0100

    update android build-tools

--- android/android_install.sh
+++ android/android_install.sh
@@ -6,34 +6,36 @@ mkdir -pv ~/android
 cd ~/android
 
 # NDK
+NDK="android-ndk-r10e-linux-x86_64.bin"
 if [ ! -d android-ndk-r10e ]; then
-	if [ ! -f android-ndk-r10e-linux-x86_64.bin ]; then
+	if [ ! -f $NDK ]; then
 		echo "Download android ndk..."
-		wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+		wget http://dl.google.com/android/ndk/${NDK}
 	fi
-	if ! md5sum -c <(echo "19af543b068bdb7f27787c2bc69aba7f  android-ndk-r10e-linux-x86_64.bin"); then
-		rm *.bin
+	if ! md5sum -c <(echo "19af543b068bdb7f27787c2bc69aba7f  ${NDK}"); then
+		rm ${NDK}
 		exit 1
 	fi
 	echo "Install android ndk..."
-	chmod a+x android-ndk-r10e-linux-x86_64.bin
-	./android-ndk-r10e-linux-x86_64.bin
-	rm android-ndk-r10e-linux-x86_64.bin
+	chmod a+x ${NDK}
+	./${NDK}
+	rm ${NDK}
 fi
 
 # SDK
+SDK="android-sdk_r24.4.1-linux.tgz"
 if [ ! -d android-sdk-linux ]; then
-	if [ ! -f android-sdk_r24.4.1-linux.tgz ]; then
+	if [ ! -f ${SDK} ]; then
 		echo "Download android sdk..."
-		wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+		wget http://dl.google.com/android/${SDK}
 	fi
-	if ! md5sum -c <(echo "978ee9da3dda10fb786709b7c2e924c0  android-sdk_r24.4.1-linux.tgz"); then
-		rm *.tgz
+	if ! md5sum -c <(echo "978ee9da3dda10fb786709b7c2e924c0  ${SDK}"); then
+		rm ${SDK}
 		exit 2
 	fi
 	echo "Install android sdk..."
-	tar xzvf android-sdk_r24.4.1-linux.tgz
-	rm android-sdk_r24.4.1-linux.tgz
+	tar xzvf ${SDK}
+	rm ${SDK}
 fi
 
 echo "Install all revision 19 related (android 4.4.2)"
@@ -41,7 +43,7 @@ export PATH=~/android/android-sdk-linux/tools:$PATH
 #android list sdk --extended
 expect -c ' set timeout -1;\
 spawn android - update sdk --all --no-ui --filter \
-platform-tools,build-tools-23.0.1,android-19,sys-img-armeabi-v7a-android-19;\
+platform-tools,build-tools-23.0.2,android-19,sys-img-armeabi-v7a-android-19;\
 expect "Do you accept the license" { exp_send "y\r";exp_continue } '
 
 echo "Please add this to your bashrc..."
