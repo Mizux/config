f302a5ebbb360684b6f150ad1ac61921e11ffac5
mizux <dev@mizux.net>
2015-11-24 16:33:17 +0100

    update nodejs install.

--- js/js_install.sh
+++ js/js_install.sh
@@ -1,24 +1,30 @@
-#!/bin/env bash
-echo "Install nodejs and ruby..."
-yaourt -S --needed nodejs ruby
-echo "Install nodejs and ruby...DONE"
+#!/usr/bin/zsh
 
-echo "Install env to .zshrc..."
-npmdir="${HOME}/.npm-packages"
-envfix='
-NPM_PACKAGES="%s"
-NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-PATH="$NPM_PACKAGES/bin:$PATH"
-# Unset manpath so we can inherit from /etc/manpath via the `manpath`
-# command
-unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
-MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-'
-printf "${envfix}" ${npmdir} >> ~/.zshrc
-echo "Install env to .zshrc...DONE"
+#echo "Install nodejs and ruby..."
+#yaourt -S --needed nodejs ruby
+#echo "Install nodejs and ruby...DONE"
 
-echo "Install npm modules..."
+echo "Install NodeJS 5.1.0 ..."
+cd /tmp
+wget https://nodejs.org/dist/latest/node-v5.1.0-linux-x64.tar.gz
+mkdir -pv ~/.nodejs
+tar xzvf node-*-linux-x64.tar.gz -C ~/.nodejs/
+rm node-*-linux-x64.tar.gz
+ln -fs ~/.nodejs/node-v5.1.0-linux-x64 ~/.nodejs/current
+
+echo -e "\n# NodeJS" >> ~/.zshrc
+echo 'export NPM_PACKAGES=~/.npm-packages' >> ~/.zshrc
+echo 'export NODE_PATH=$NPM_PACKAGES/lib/node_modules:$NODE_PATH' >> ~/.zshrc
+echo 'export PATH=$NPM_PACKAGES/bin:~/.nodejs/current/bin:$PATH' >> ~/.zshrc
+echo 'unset MANPATH; export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"' >> ~/.zshrc
 source ~/.zshrc
-npm install -g yo karma bower grunt-cli phantomjs jshint # yeoman
-npm install -g generator-angular generator-webapp
-echo "Install npm modules...DONE"
+npm config set prefix `echo $NPM_PACKAGES`
+echo "Install NodeJS ...DONE"
+
+echo "Please call: source ~/.zshrc"
+
+#echo "Install npm modules..."
+#npm install -g gulp
+#npm install -g karma bower grunt-cli phantomjs jshint
+#npm install -g yo generator-angular generator-webapp
+#echo "Install npm modules...DONE"
