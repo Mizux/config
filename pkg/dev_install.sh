20f374e4878f224628d85181f2a00e0b60a37a37
mizux <dev@mizux.net>
2015-11-24 22:13:33 +0100

    update dev-tools

--- pkg/dev_install.sh
+++ pkg/dev_install.sh
@@ -4,12 +4,15 @@ echo "update System..."
 pacman -Syu
 echo "update System Done"
 
-echo "install Dev..."
-pacman -S --needed git mercurial tk tig aspell-en aspell-fr meld sloccount \
-qtcreator ipython clang cmake ninja gdb strace valgrind \
+echo "install Dev tools..."
+pacman -S --needed base-devel\
+git tk tig mercurial aspell-en aspell-fr meld sloccount \
+doxygen graphviz ctags \
+upx \
+qtcreator ipython cmake ninja clang gcc gdb strace valgrind cppcheck \
+echo "install  tools..."
 boost blas lapack eigen opencv lua \
-bullet box2d openscenegraph qt qt4 qt5-doc \
+bullet box2d openscenegraph qt qt5-doc qt4 \
 sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf \
-doxygen graphviz ctags upx \
 desmume
 echo "install Dev Done"
