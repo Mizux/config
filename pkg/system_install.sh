826336b38a4641f44a645e5da3ab3e6c9783bc33
Mizux <dev@mizux.net>
2016-02-17 09:20:17 +0100

    Update system_install.sh

--- pkg/system_install.sh
+++ pkg/system_install.sh
@@ -9,13 +9,13 @@ pacman -S --needed base base-devel namcap zsh gvim \
 dialog sudo \
 udevil udisks ntfs-3g dosfstools ncdu rsync \
 dialog perl-rename bc tree htop hwinfo acpi acpid openssh wget \
-xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics mesa xf86-video-intel \
+xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics xf86-video-intel \
 nvidia nvidia-utils nvidia-libgl \
 awesome rxvt-unicode rox thunar thunar-archive-plugin file-roller \
 lxappearance gtk-engine-murrine gtk-engine-aurora \
 gnome-icon-theme gnome-icon-theme-extras \
 oxygen-icons oxygen-icons-svg oxygen-gtk2 \
-anthy uim otf-ipafont ttf-hanazono \
+ibus ibus-qt ibus-anthy otf-ipafont ttf-hanazono \
 ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-freefont
 echo "install System Done"
 
