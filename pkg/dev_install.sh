88220b244ac5f616dcec791276a5a58b896715da
Mizux <dev@mizux.net>
2016-03-16 10:32:46 +0100

    Add clang-analyzer + refactoring

--- pkg/dev_install.sh
+++ pkg/dev_install.sh
@@ -6,12 +6,14 @@ echo "update System Done"
 
 echo "install Dev tools..."
 pacman -S --needed base-devel \
-git tk tig mercurial aspell-en aspell-fr meld sloccount \
-doxygen graphviz ctags \
-upx \
-qtcreator ipython cmake ninja clang gcc gdb strace valgrind cppcheck
-echo "install  tools..."
+qtcreator ipython \
+git tk tig mercurial aspell-en aspell-fr meld \
+upx strace valgrind cppcheck \
+doxygen graphviz ctags sloccount \
+cmake ninja gcc gdb clang clang-analyzer
+echo "install Dev Done"
+echo "install 3rd Party..."
 pacman -S --needed \
 boost blas lapack eigen opencv lua \
 bullet box2d openscenegraph qt qt5-doc qt4
-echo "install Dev Done"
+echo "install 3rd Party Done"
