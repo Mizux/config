88d063169362d6f75efdb9b7b3b12f14a5ed0f8e
Mizux <dev@mizux.net>
2016-04-05 11:14:34 +0200

    Update system_install.sh

--- pkg/system_install.sh
+++ pkg/system_install.sh
@@ -9,8 +9,8 @@ pacman -S --needed base base-devel namcap zsh gvim \
 dialog sudo \
 udevil udisks ntfs-3g dosfstools ncdu rsync \
 dialog perl-rename bc tree htop hwinfo acpi acpid openssh wget \
-xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics xf86-video-intel \
-nvidia nvidia-utils nvidia-libgl \
+xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics \
+xf86-video-nouveau xf86-video-intel \
 awesome xlockmore rxvt-unicode rox thunar thunar-archive-plugin file-roller \
 lxappearance gtk-engine-murrine gtk-engine-aurora \
 gnome-icon-theme gnome-icon-theme-extras \
