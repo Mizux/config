326fbac1c7b81486b08cda42751e7ecc2f96e442
Mizux <dev@mizux.net>
2016-05-13 17:42:45 +0200

    update nodejs

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/zsh
 
-#echo "Install nodejs and ruby..."
-#yaourt -S --needed nodejs ruby
-#echo "Install nodejs and ruby...DONE"
+#echo "Install nodejs..."
+#yaourt -S --needed nodejs
+#echo "Install nodejs...DONE"
 
-echo "Install NodeJS 5.1.0 ..."
+echo "Install NodeJS 6.1.0..."
 cd /tmp
-wget https://nodejs.org/dist/latest/node-v5.1.0-linux-x64.tar.gz
+wget https://nodejs.org/dist/latest/node-v6.1.0-linux-x64.tar.gz
 mkdir -pv ~/.nodejs
 tar xzvf node-*-linux-x64.tar.gz -C ~/.nodejs/
 rm node-*-linux-x64.tar.gz
-ln -fs ~/.nodejs/node-v5.1.0-linux-x64 ~/.nodejs/current
+ln -fs ~/.nodejs/node-v6.1.0-linux-x64 ~/.nodejs/current
 
-echo -e "\n# NodeJS" >> ~/.zshrc
-echo 'export NPM_PACKAGES=~/.npm-packages' >> ~/.zshrc
-echo 'export NODE_PATH=$NPM_PACKAGES/lib/node_modules:$NODE_PATH' >> ~/.zshrc
-echo 'export PATH=$NPM_PACKAGES/bin:~/.nodejs/current/bin:$PATH' >> ~/.zshrc
-echo 'unset MANPATH; export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"' >> ~/.zshrc
+if grep --quiet "# NodeJS" ~/.zshrc; then
+	echo "archlinux repo already here"
+else
+	echo -e "\n# NodeJS" >> ~/.zshrc
+	echo 'export NPM_PACKAGES=~/.npm-packages' >> ~/.zshrc
+	echo 'export NODE_PATH=$NPM_PACKAGES/lib/node_modules:$NODE_PATH' >> ~/.zshrc
+	echo 'export PATH=$NPM_PACKAGES/bin:~/.nodejs/current/bin:$PATH' >> ~/.zshrc
+	echo 'unset MANPATH; export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"' >> ~/.zshrc
+fi
 source ~/.zshrc
 npm config set prefix `echo $NPM_PACKAGES`
 echo "Install NodeJS ...DONE"
-
-echo "Please call: source ~/.zshrc"
-
-echo "Install npm modules..."
-npm install -g gulp
-#npm install -g karma bower grunt-cli phantomjs jshint
-#npm install -g yo generator-angular generator-webapp
-npm list -g --depth=0
-echo "Install npm modules...DONE"
