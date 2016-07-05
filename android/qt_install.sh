c7ed1d285743760eb433a0cedbacb6607958af8e
Mizux <dev@mizux.net>
2016-07-05 22:09:27 +0200

    Android: Update Qt installer script

--- android/qt_install.sh
+++ android/qt_install.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 # QT
+INSTALLER="qt-unified-linux-x64-online.run"
+INSTALLER_MD5="77a0f5fa86d648869b64229fb461c9f7"
 if [ ! -d ~/Qt ]; then
 	cd /tmp
 	if [ ! -f qt-opensource-linux-x64-online.run ]; then
 		echo "Download qt..."
-		wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
-		echo "ffce5589bf0c8b814daab32894dd6450  qt-unified-linux-x64-online.run" > check.txt
+		wget http://download.qt.io/official_releases/online_installers/${INSTALLER}
+		echo "${INSTALLER_MD5}  ${INSTALLER}" > check.txt
 		if ! md5sum -c check.txt; then
 			rm check.txt
-			rm *.run
+			rm ${INSTALLER}
 			exit 1
 		fi
 		rm check.txt
 	fi
 	echo "Install qt..."
-	chmod a+x qt-unified-linux-x64-online.run
-	./qt-unified-linux-x64-online.run
-	rm qt-unified-linux-x64-online.run
+	chmod a+x ${INSTALLER}
+	./${INSTALLER}
+	rm ${INSTALLER}
 fi
 
 echo "Please add this to your zshrc..."
 echo "# QT"
-echo "export ANDROID_QT_ROOT=~/Qt/5.5/android_armv7"
+echo "export ANDROID_QT_ROOT=~/Qt/5.7/android_armv7"
+echo "export PATH=~/Qt/5.7/gcc_64/bin:$PATH"
 echo "export PATH=~/Qt/Tools/QtCreator/bin:$PATH"
-
-# Android/QT CMake
-
 
 echo "SUCCESS !"
