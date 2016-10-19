9c4d41343e67bb97d4f79e7274c6ccd0fe6d5fc0
Mizux Seiha <dev@mizux.net>
2016-10-19 12:40:28 +0200

    remove android stuff

--- android/pkg_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "update System..."
-sudo pacman -Syu
-echo "update System Done"
-
-echo "install Dev..."
-sudo pacman -S --needed base jdk8-openjdk jre8-openjdk
-echo "install Dev Done"
