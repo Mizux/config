b6f322ac9cc4f53fe0e6093061d75d8c2b18d6ae
Mizux Seiha <dev@mizux.net>
2016-10-19 12:40:37 +0200

    remove nodejs stuff

--- nodejs/modules.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-echo "Install npm modules..."
-npm install -g gulp bower
-#npm install -g karma bower grunt-cli phantomjs jshint
-#npm install -g yo generator-angular generator-webapp
-npm list -g --depth=0
-echo "Install npm modules...DONE"
