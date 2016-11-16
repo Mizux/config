790fb32f7a0ceb52666844ebe622c4dded794247
Mizux Seiha <dev@mizux.net>
2016-11-16 17:34:23 +0100

    [WIP] add qtcreator style

---
+++ qtcreator/README.md
@@ -0,0 +1,7 @@
+# Description
+My custom style for QTCreator:
+## Mouse
+Inspired by the hack"Mouse" character from Armitage III...
+
+## Forest
+Inspired by Mother Earth.
---
+++ qtcreator/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing qtcreator styles..."
+set -x
+mkdir -pv ~/config/QtProject/qtcreator/style
+cp mouse.xml ~/config/QtProject/qtcreator/style/
+
+
