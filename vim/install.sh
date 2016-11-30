506d18de1ad43cb6767ff43e2013978eebf4c159
Mizux Seiha <dev@mizux.net>
2016-11-30 16:47:42 +0100

    update vim install

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,10 @@ rm -rf ~/.vim
 rm -f ~/.vimrc
 
 cp vimrc ~/.vimrc
+
+mkdir -p ~/.vim/colors
+cp mouse.vim ~/.vim/colors/
+
 mkdir -p ~/.vim/bundle && \
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
