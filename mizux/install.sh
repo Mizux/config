346faab327c423c39b04a11db50ec4bc85af8ceb
Mizux Seiha <dev@mizux.net>
2016-12-07 09:22:37 +0100

    mizux: install script use set -x

--- mizux/install.sh
+++ mizux/install.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Warning !\nThis script won't do backup !"
+echo -e "/!\ Warning /!\ This script won't do backup !"
 
-echo "Installing zsh configuration..."
+set -x
 cp zshrc ~/.zshrc
-echo "Installing zsh configuration DONE"
 
-echo "Installing git configuration..."
 cp gitconfig ~/.gitconfig
-echo "Installing git configuration DONE"
 
-echo "Installing X configuration..."
 cp Xdefaults ~/.Xdefaults
 cp xinitrc ~/.xinitrc
 cp xsession ~/.xsession
 cp gtkrc-2.0 ~/.gtkrc-2.0
-echo "Installing X configuration DONE"
 
-echo "Installing awesome configuration..."
 mkdir -pv ~/.config/awesome
 cp -r rc.lua ~/.config/awesome/rc.lua
-echo "Installing awesome configuration DONE"
 
-
-echo "Installing builder..."
 mkdir -pv ~/.local/bin
 cp bgc bgm bcc bcm bcf ~/.local/bin
-echo "Installing builder DONE"
