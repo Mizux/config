5712b47ebf4e2cfc8031edfc1478438694456cc7
Mizux Seiha <mizux.dev@gmail.com>
2020-03-18 13:29:10 +0100

    Add evince and pdftk
    * evince needed for pdf print preview
    * pdftk to rotate .pdf file
      usage: pdftk <in.pdf> rotate 1-east output <out.pdf>

--- pkg/media_install.sh
+++ pkg/media_install.sh
@@ -9,7 +9,8 @@ pacman -S --needed unrar unzip p7zip \
 chromium firefox flashplugin filezilla hexchat perl-file-mimeinfo \
 smplayer vlc audacious audacious-plugins libcue libmms neon wavpack alsa-utils  \
 scrot gimp inkscape feh gcolor2 blender wings3d \
-calibre libreoffice zathura zathura-pdf-mupdf zathura-djvu \
+calibre libreoffice \
+zathura zathura-pdf-mupdf zathura-djvu evince pdftk \
 nethack wesnoth dwarffortress armagetronad
 yaourt -S --needed --noconfirm chromium-pepper-flash
 echo "install Media Done"
