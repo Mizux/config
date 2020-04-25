9675292f847be0eb3237a11b9ed34f5c945620be
Mizux Seiha <mizux.dev@gmail.com>
2020-04-25 15:39:06 +0200

    Update mizux

--- mizux/install.sh
+++ mizux/install.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
 # /!\ This script won't do backup /!\
-set -x
+set -euxo pipefail
+
+# Shell
 cp zshrc ~/.zshrc
 
+# Git
 cp gitconfig ~/.gitconfig
 
+# X11
+#cp xinitrc ~/.xinitrc
+#cp xsession ~/.xsession
+#cp gtkrc-2.0 ~/.gtkrc-2.0
+
+## Awesome WM
+mkdir -pv ~/.config/awesome && cp rc.lua ~/.config/awesome
+
+## urxvt
 cp Xdefaults ~/.Xdefaults
 mkdir -pv ~/.urxvt/ext && cp resize-font ~/.urxvt/ext
 
-cp xinitrc ~/.xinitrc
-cp xsession ~/.xsession
-cp gtkrc-2.0 ~/.gtkrc-2.0
-
-mkdir -pv ~/.config/awesome && cp rc.lua ~/.config/awesome
-
 # Build tools
-mkdir -pv ~/.local/bin
-cp qc qm qf ~/.local/bin
+#mkdir -pv ~/.local/bin
+#cp qc qm qf ~/.local/bin
 
-# Plantuml
-mkdir -pv ~/.local/bin
-cp plantuml ~/.local/bin
-(cd ~/.local/bin && wget https://downloads.sourceforge.net/project/plantuml/plantuml.jar)
+## Plantuml
+#mkdir -pv ~/.local/bin
+#cp plantuml ~/.local/bin
+#(cd ~/.local/bin && wget https://downloads.sourceforge.net/project/plantuml/plantuml.jar)
