dc765940c04b22eb1fce570c078e54c457963a10
Mizux Seiha <mizux.dev@gmail.com>
2020-04-25 17:06:11 +0200

    Update vim

--- vim/install.sh
+++ vim/install.sh
@@ -8,7 +8,7 @@ rm -f ~/.vimrc
 cp vimrc ~/.vimrc
 
 mkdir -p ~/.vim/colors
-cp mouse.vim ~/.vim/colors/
+#cp mouse.vim ~/.vim/colors/
 
 mkdir -p ~/.vim/bundle && \
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
