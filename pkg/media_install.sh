095f8dc901a09d4c495ac2e96c1f22c5f4fe0e07
Mizux Seiha <mizux.dev@gmail.com>
2020-04-25 20:53:44 +0200

    Update pkg

--- pkg/media_install.sh
+++ pkg/media_install.sh
@@ -10,7 +10,7 @@ unrar unzip p7zip \
 chromium firefox youtube-dl \
 hexchat \
 audacious audacious-plugins audacity \
-scrot feh gimp inkscape gcolor2 \
+scrot feh gimp inkscape gcolor2 gcolor3 \
 smplayer vlc ffmpeg \
 blender wings3d \
 zathura zathura-pdf-mupdf zathura-djvu pdftk
--- pkg/system_install.sh
+++ pkg/system_install.sh
@@ -12,40 +12,43 @@ perl-rename \
 tree ncdu rsync \
 htop hwinfo \
 wget curl \
-alsa-utils \
+alsa-utils
+echo "install System Done"
+
+echo "install Xorg..."
+pacman -S --needed \
 xorg-server xorg-xinit \
 awesome rxvt-unicode rox file-roller \
+lxappearance \
+gtk-engines gtk-engine-murrine \
+gnome-icon-theme gnome-icon-theme-extras \
+oxygen-icons oxygen-icons-svg oxygen-gtk2 \
 ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-freefont
-echo "install System Done"
+echo "install Xorg Done"
 
 exit
 
 # Deprecated
-#vim-spell-fr vim-spell-en \
-#namcap \
-#dialog \
-#sudo \
-#udevil \
-#udisks \
-#ntfs-3g  \
-#dialog \
-#perl-rename \
-#bc \
-#sysstat \
-#acpi acpid \
-#openssh \
-#wget \
+vim-spell-fr vim-spell-en \
+namcap \
+dialog \
+sudo \
+udevil \
+udisks \
+ntfs-3g  \
+bc \
+sysstat \
+acpi acpid \
+openssh \
 
-#xorg-server-utils \
-#xf86-input-synaptics \
-#xf86-video-nouveau \
-#xf86-video-intel \
+xorg-server-utils \
+xf86-input-synaptics \
+xf86-video-nouveau \
+xf86-video-intel \
 
-#awesome \
-#xlockmore \
-#thunar thunar-archive-plugin \
-#lxappearance \
-#gtk-engine-murrine gtk-engine-aurora \
-#gnome-icon-theme gnome-icon-theme-extras \
-#oxygen-icons oxygen-icons-svg oxygen-gtk2 \
-#ibus ibus-qt ibus-anthy otf-ipafont ttf-hanazono \
+xlockmore \
+thunar thunar-archive-plugin \
+lxappearance-gtk3 \
+
+# Japanese Input Support
+ibus ibus-qt ibus-anthy otf-ipafont ttf-hanazono \
