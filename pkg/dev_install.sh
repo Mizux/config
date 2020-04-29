04bc1f71d6905fd473848cfdadb3984e13070f8d
Mizux Seiha <mizux.dev@gmail.com>
2020-04-29 13:55:21 +0200

    pkg: Update dev

--- pkg/dev_install.sh
+++ pkg/dev_install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+set -euxo pipefail
 
 echo "update System..."
 pacman -Syu
@@ -7,21 +8,43 @@ echo "update System Done"
 echo "install Dev tools..."
 pacman -S --needed \
 base-devel shellcheck \
+man-pages man-db \
 gvim aspell-en aspell-fr \
 git tk tig \
 meld \
-upx strace valgrind ctags \
-cmake ninja gcc gdb clang \
-qtcreator qt5 qt5-doc \
-ipython python python-pip python-virtualenv \
-code nodejs npm \
-jdk-openjdk maven \
-dotnet-sdk \
-doxygen graphviz plantuml \
-lua swig \
-opencv \
-bullet box2d
+graphviz plantuml
 echo "install Dev Done"
+
+echo "install C++..."
+pacman -S --needed \
+qtcreator qt5 qt5-doc \
+doxygen \
+upx strace valgrind ctags \
+cmake ninja \
+gcc gdb clang \
+lua swig \
+opencv bullet box2d
+echo "install c++ Done"
+
+echo "install Python..."
+pacman -S --needed \
+ipython python python-pip python-virtualenv
+echo "install Python Done"
+
+echo "install JS..."
+pacman -S --needed \
+code nodejs npm
+echo "install JS Done"
+
+echo "install Java..."
+pacman -S --needed \
+jdk-openjdk maven
+echo "install Java Done"
+
+echo "install .Net..."
+pacman -S --needed \
+dotnet-sdk
+echo "install .Net Done"
 
 echo "install Docker..."
 pacman -S --needed \
