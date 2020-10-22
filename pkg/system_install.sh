e5f58f0730043f632565a6224ab76fbdb7c848e3
Mizux Seiha <mizux.dev@gmail.com>
2020-10-22 14:06:26 +0200

    system: add numlockx

--- pkg/system_install.sh
+++ pkg/system_install.sh
@@ -19,7 +19,7 @@ echo "install System Done"
 # Xorg server and WM
 echo "install Xorg..."
 pacman -S --needed \
-xorg-server xorg-xinit xterm \
+xorg-server xorg-xinit xterm numlockx \
 xf86-video-intel xf86-video-fbdev xf86-video-vesa \
 awesome rxvt-unicode rox file-roller \
 breeze-gtk breeze-icons \
