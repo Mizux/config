18b1100dc4bee6f5dba38a994f33116b403bb22b
Corentin Le Molgat <corentinl@google.com>
2022-10-19 09:02:17 +0200

    vscode: add few extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,20 +7,42 @@ rm -rf "$HOME/.config/Code - OSS"
 rm -rf "$HOME/.vscode-oss"
 
 # Install my extensions
+
+## IDE
+code --install-extension alefragnani.Bookmarks
+code --install-extension GitHub.github-vscode-theme
+code --install-extension PKief.material-icon-theme
+code --install-extension vscodevim.vim
+code --install-extension eamodio.gitlens
+
+## Language
+code --install-extension esbenp.prettier-vscode
+
+### C++
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.cmake-tools
+code --install-extension twxs.cmake
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension jeff-hykin.better-cpp-syntax
+
+### Dart / Flutter
 code --install-extension Dart-Code.dart-code
 code --install-extension Dart-Code.flutter
-code --install-extension esbenp.prettier-vscode
-code --install-extension GitHub.github-vscode-theme
+
+### Golang
 code --install-extension golang.go
-code --install-extension llvm-vs-code-extensions.vscode-clangd
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cmake-tools
-code --install-extension PKief.material-icon-theme
+
+### Java
 code --install-extension redhat.java
+
+### Python
+code --install-extension ms-python.python
+
+### Web
 code --install-extension ritwickdey.LiveServer
-code --install-extension twxs.cmake
-code --install-extension vscodevim.vim
+code --install-extension ms-azuretools.vscode-docker
 
 # Copy my default settings
 mkdir -pv "$HOME/.config/Code - OSS/User/"
