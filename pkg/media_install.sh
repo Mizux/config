01c70b6c3c7ae7314eeed69208516e2bb46f77f7
Mizux Seiha <mizux.dev@gmail.com>
2024-10-29 01:35:29 +0100

    update pkg/media_install

--- pkg/media_install.sh
+++ pkg/media_install.sh
@@ -10,20 +10,19 @@ unrar unzip p7zip \
 chromium firefox yt-dlp \
 hexchat \
 cmus audacious audacious-plugins \
-audacity kid3-qt \
+audacity kid3-qt furnace \
 scrot feh gimp xcftools inkscape synfigstudio synfig \
-gcolor2 gcolor3 \
+gcolor3 \
 smplayer vlc ffmpeg \
-blender wings3d goxel \
+blender goxel \
 zathura zathura-pdf-mupdf zathura-djvu pdftk \
-calibre mcomix mupdf-tools
+calibre mupdf-tools
 echo "install Media Done"
 
 echo "install Game..."
 pacman -S --needed \
 nethack dwarffortress wesnoth \
-xonotic minetest \
-steam-native-runtime
+xonotic minetest steam \
 # dopewars armagetronad
 echo "install Game Done"
 
@@ -36,6 +35,9 @@ obs-studio
 # A multi-system chiptune tracker
 # shellcheck disable=SC2317
 furnace
+
+# aur
+wings3d mcomix
 
 # Deprecated
 # shellcheck disable=SC2317
