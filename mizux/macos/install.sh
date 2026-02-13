5d8196c6e9d038b8435ea8164451508ca0ba4d7e
Mizux Seiha <mizux.dev@gmail.com>
2026-02-13 16:09:32 +0100

    mizux(macos): add brew setup

--- mizux/macos/install.sh
+++ mizux/macos/install.sh
@@ -3,6 +3,17 @@
 # /!\ This script won't do backup /!\
 set -euxo pipefail
 
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install bash zsh bat eza htop tree wget
+brew install btop ncdu fastfetch
+brew install git tig git-gui cmake make ccache
+
+# brew install dart-sdk
+# brew install dotnet-sdk
+# brew install openjdk
+# brew install openssl@1.1 repo
+
 # Shell
 cp zshrc ~/.zshrc
 
