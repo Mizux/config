07773b65eed068f154bcb5c5b279bbcb297e3e46
Corentin Le Molgat <corentinl@google.com>
2026-02-18 17:37:12 +0100

    helix: add mouse color theme.

--- helix/install.sh
+++ helix/install.sh
@@ -7,3 +7,6 @@ rm -rf "$HOME"/.config/helix
 # Install conf
 mkdir -p "$HOME"/.config/helix
 cp config.toml "$HOME"/.config/helix/
+
+cp languages.toml "$HOME"/.config/helix/
+cp -r themes "$HOME"/.config/helix/
