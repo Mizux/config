8f977468172b3118f8e7fc56bdb7837a83061b14
Mizux Seiha <mizux.dev@gmail.com>
2026-09-23 14:16:18 +0200

    macos: add lazygit

--- mizux/macos/install.sh
+++ mizux/macos/install.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install bash zsh bat eza htop tree wget
 brew install btop ncdu fastfetch
-brew install git tig git-gui cmake make ccache
+brew install git tig lazygit git-gui cmake make ccache
 brew install font-dejavu-sans-mono-nerd-font
 
 brew install helix
