82ba2569a06d9648be970890de6a7726a658b322
Mizux Seiha <mizux.dev@gmail.com>
2026-09-23 14:30:54 +0200

    dir layout refactor

--- distros/macos/install.sh
+++ distros/macos/install.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-
 # /!\ This script won't do backup /!\
 set -euxo pipefail
 
-
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install bash zsh bat eza htop tree wget
-brew install btop ncdu fastfetch
-brew install git tig lazygit git-gui cmake make ccache
+brew install kitty bash zsh bat eza tree wget
+brew install htop btop ncdu fastfetch
+brew install git tig lazygit git-gui repo
+brew install cmake make ccache
 brew install font-dejavu-sans-mono-nerd-font
 
-brew install helix
+brew install helix neovim meld
+brew install gimp blockbench
 
+# brew install qt
+# brew install ncurses
 # brew install dart-sdk
 # brew install dotnet-sdk
-# brew install openjdk
+# brew install openjdk maven
 # brew install openssl@1.1 repo
-
-# Shell
-cp zshrc ~/.zshrc
-
-# Git
-cp gitconfig ~/.gitconfig
